7e333108568e72decb6dd1ccaf66d7189196a7fc
t4traw <t4traw@gmail.com>
2017-05-12 16:45:02 +0900

    getのテスト項目の追加 && hashの書き方を新しい記法に変更

--- test/rms_item_api/item_test.rb
+++ test/rms_item_api/item_test.rb
@@ -5,42 +5,58 @@ class RmsItemApiTest < Minitest::Test
   include TestHelper::Client
 
   def test_item_get
+    inventory_data = {
+      "itemInventory"=>{
+        "inventoryType"=>"1",
+        "inventories"=>{
+          "inventory"=>{
+            "inventoryCount"=>"0",
+            "isBackorder"=>"false",
+            "isRestoreInventoryFlag"=>"false"
+          }
+        },
+        "inventoryQuantityFlag"=>"0"
+      }
+    }
     VCR.use_cassette('item/test_item_get') do
-      assert_equal 'test_item', client.get('test1234').item_name
+      item = client.get('test1234')
+      assert_equal true, item.is_success?
+      assert_equal 'test_item', item.item_name
+      assert_equal inventory_data, item.item_inventory
     end
   end
 
   def test_item_insert
     insert_data = {
-      :itemUrl=>"test1234",
-      :itemNumber=>"test1234",
-      :itemName=>"test_item",
-      :itemPrice=>12345,
-      :genreId=>101916,
-      :catalogIdExemptionReason=>5,
-      :descriptionForPC=>"Lorem ipsum",
-      :descriptionForMobile=>"Lorem ipsum",
-      :descriptionForSmartPhone=>"Lorem ipsum",
-      :catchCopyForPC=>"",
-      :catchCopyForMobile=>"",
-      :descriptionBySalesMethod=>"Lorem ipsum",
-      :isSaleButton=>true,
-      :isDocumentButton=>false,
-      :isInquiryButton=>true,
-      :isStockNoticeButton=>false,
-      :itemLayout=>1,
-      :isIncludedTax=>true,
-      :isIncludedPostage=>false,
-      :isIncludedCashOnDeliveryPostage=>false,
-      :isNoshiEnable=>false,
-      :isUnavailableForSearch=>false,
-      :isDepot=>true,
-      :detailSellType=>0,
-      :item_inventory=>{
-        :inventory_type=>1,
-        :inventories=>{
-          :inventory=>{
-            :inventoryCount=>0
+      itemUrl: "test1234",
+      itemNumber: "test1234",
+      itemName: "test_item",
+      itemPrice: 12345,
+      genreId: 101916,
+      catalogIdExemptionReason: 5,
+      descriptionForPC: "Lorem ipsum",
+      descriptionForMobile: "Lorem ipsum",
+      descriptionForSmartPhone: "Lorem ipsum",
+      catchCopyForPC: "",
+      catchCopyForMobile: "",
+      descriptionBySalesMethod: "Lorem ipsum",
+      isSaleButton: true,
+      isDocumentButton: false,
+      isInquiryButton: true,
+      isStockNoticeButton: false,
+      itemLayout: 1,
+      isIncludedTax: true,
+      isIncludedPostage: false,
+      isIncludedCashOnDeliveryPostage: false,
+      isNoshiEnable: false,
+      isUnavailableForSearch: false,
+      isDepot: true,
+      detailSellType: 0,
+      item_inventory: {
+        inventory_type: 1,
+        inventories: {
+          inventory: {
+            inventoryCount: 0
           }
         }
       }
