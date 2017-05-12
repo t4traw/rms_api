eb926fddba12ffefa299540bdfb35b6af5ce516f
t4traw <t4traw@gmail.com>
2017-05-12 16:33:24 +0900

    テスト名をぷちfix🐾

--- test/rms_item_api/item_test.rb
+++ test/rms_item_api/item_test.rb
@@ -4,8 +4,8 @@ require 'test_helper'
 class RmsItemApiTest < Minitest::Test
   include TestHelper::Client
 
-  def test_get
-    VCR.use_cassette('get/test_get') do
+  def test_item_get
+    VCR.use_cassette('get/test_item_get') do
       assert_equal 'test_item', client.get('test1234').item_name
     end
   end
